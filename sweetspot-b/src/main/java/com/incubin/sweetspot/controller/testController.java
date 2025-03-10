a09bdaabacf1ccfeaabb0949d94f5144c95b4313
Yeojin0523 <d02jyjeong@naver.com>
2025-03-11 00:59:03 +0900

    sweetspot 초기 백/프론트 코드

---
+++ sweetspot-b/src/main/java/com/incubin/sweetspot/controller/testController.java
@@ -0,0 +1,20 @@
+package com.incubin.sweetspot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class testController {
+
+    @GetMapping("/message")
+    public Map<String, String> getMessage() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from Spring Boot!");
+        return response;
+    }
+}
---
+++ sweetspot-b/src/test/java/com/incubin/sweetspot/SweetspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubin.sweetspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
